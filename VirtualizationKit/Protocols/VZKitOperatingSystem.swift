ac53cfd1a9573576e00ddfc7730a8be4191e100f
iOmega8561 <giusepperocco38@gmail.com>
2024-10-12 15:26:05 +0200

    Fixed leftovers from transition and forced framework identifier for translations

--- VirtualizationKit/Protocols/VZKitOperatingSystem.swift
+++ VirtualizationKit/Protocols/VZKitOperatingSystem.swift
@@ -13,7 +13,7 @@ import SwiftUI
 ///    This protocol defines how a Virtual Machine OS Type should be implemented in this application.
 ///    `VirtHandlerMachineOS` helps us define two crucial methods that will significally reduce
 ///    the bulk of `SwiftUI` statements. We can simply get the assets of the given OS by callin these methods.
-public protocol VZKitOperatingSystem: Codable, Hashable, CaseIterable {
+public protocol VZKitOperatingSystem: Codable, Hashable, CaseIterable, Sendable {
     
     var image: Image { get }
     
