fb3eaa0caa2e0b360fe6e4d06fc3155064654ddb
iOmega8561 <giusepperocco38@gmail.com>
2024-10-14 21:11:46 +0200

    Introduced docstrings and added constant for max apple vms

--- VirtualizationKit/CoreDefinitions/VirtualizationKit.swift
+++ VirtualizationKit/CoreDefinitions/VirtualizationKit.swift
@@ -18,17 +18,32 @@ import Foundation
 
 public final class _VirtualizationKit: Sendable {
     
+    /// The version string of this framework
     public let version: String = "1.0"
     
+    /// The maximum amount of macOS virtual machines that can run simultaneously.
+    /// Unfortunately Apple Virtualization Framework limits this amount to two VMs at once :(
+    public let appleMaxVMs: Int = 2
+    
+    /// The resource bundle of this framework,
+    /// to get assets and localized strings from here and not the main bundle
     public let bundle: Bundle? = .init(identifier: "giusepperocco.VirtualizationKit")
     
-    @MainActor private var _bundlePath: String = NSHomeDirectory() + "/VirtualizationKit.bundle/"
+    /// The host machine path where all the VM data should be located.
+    /// The application that uses this framework should set this value to something else.
+    /// This property will only be used internally.
+    ///
+    /// - Important: Pinned to `@MainActor` for thread safe access and `Sendable` conformance.
+    @MainActor private(set) var bundlePath: String = NSHomeDirectory() + "/VirtualizationKit.bundle/"
     
-    @MainActor public var bundlePath: String { return _bundlePath }
-    
-    @MainActor public func setBundlePath(_ bundlePath: String) {
-        _bundlePath = bundlePath
-    }
+    /// The public setter method for `bundlePath`. The application that wants to override
+    /// `bundlePath` needs to call this setter providing a `String` path
+    ///
+    /// - Important: Executes on `@MainActor` to be able to edit `bundlePath` synchronously
+    ///
+    /// - Parameters:
+    ///   - path: A `String` representing the destination path.
+    @MainActor public func setBundlePath(_ path: String) { bundlePath = path }
     
     fileprivate init() {}
 }
