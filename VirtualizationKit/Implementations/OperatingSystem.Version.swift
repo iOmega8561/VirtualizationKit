286baa861a56048da9f6cf0f1099cbb7fc781913
iOmega8561 <giusepperocco38@gmail.com>
2024-11-13 00:31:35 +0100

    Better documentation and convenience initializef

--- VirtualizationKit/Implementations/OperatingSystem.Version.swift
+++ VirtualizationKit/Implementations/OperatingSystem.Version.swift
@@ -18,19 +18,27 @@ import Virtualization
 
 extension OperatingSystem {
     
-    /// This data structure is useful to provide a standardized way to store information about
-    /// the version of the guest operating system. It conforms to:
-    /// - Equatable so that two instances can be compared easily.
-    /// - Codable is needed so that it can be easily integrated with standard storage systems.
-    /// - Sendable is useful to silence the Swift compiler. The struct is thread safe.
-    /// - Hashable so it can be integrated as enum case associated value without complications.
+    /// A data structure that standardizes the representation of a guest operating system version.
+    ///
+    /// The `Version` struct provides a consistent and structured way to store version information, including major, minor,
+    /// and patch version numbers. It conforms to several protocols to enhance its functionality and compatibility:
+    /// - `Equatable`: Allows for easy comparison of two `Version` instances.
+    /// - `Codable`: Enables seamless integration with encoding and decoding systems, such as JSON or property lists.
+    /// - `Sendable`: Ensures thread safety, allowing instances to be used across concurrent contexts without issue.
+    /// - `Hashable`: Facilitates use as an associated value in enums and in collections like dictionaries and sets.
+    ///
+    /// This struct also includes a static utility method to retrieve a `Version` instance based on a macOS restore image,
+    /// enabling streamlined extraction of version information from provided image files.
     public struct Version: VZKitOperatingSystemVersion {
         
-        /// Static utility method to retrieve an `OperatingSytem.Version` tuple, using information parsed from
-        /// the provided macOS restore image (if present). It allows to manipulate the enum cases to have them store things like OS version.
+        /// Asynchronously retrieves a `Version` instance from a specified macOS restore image URL.
         ///
-        /// - Parameters:
-        ///   - url: The URL of the installer image provided by the caller. if the image is not a macOS .ipsw the method will throw
+        /// This method loads a macOS restore image from the provided URL, then extracts version information
+        /// if the image is valid. If the image is not a macOS `.ipsw` file, or if loading fails, the method will throw an error.
+        ///
+        /// - Parameter url: The URL of the macOS installer image file. This should be an `.ipsw` image to parse successfully.
+        /// - Returns: A `Version` instance containing the parsed version information from the restore image.
+        /// - Throws: An error if the restore image could not be loaded or is invalid.
         public static func fromImage(withURL url: URL) async throws -> Self {
             
             return try await withCheckedThrowingContinuation { continuation in
@@ -42,24 +50,44 @@ extension OperatingSystem {
                         continuation.resume(throwing: error)
                         
                     case .success(let restoreImage):
-                        
-                        let version: Version = .init(
-                            major: restoreImage.operatingSystemVersion.majorVersion,
-                            minor: restoreImage.operatingSystemVersion.minorVersion,
-                            patch: restoreImage.operatingSystemVersion.patchVersion
+                        continuation.resume(
+                            returning: .init(restoreImage.operatingSystemVersion)
                         )
-                        
-                        continuation.resume(returning: version)
                     }
                 }
             }
             
         }
         
+        /// The major version number of the operating system.
         public let major: Int
         
+        /// The minor version number of the operating system.
         public let minor: Int
         
+        /// The patch version number of the operating system.
         public let patch: Int
+        
+        /// Initializes a `Version` instance with specific major, minor, and patch version numbers.
+        ///
+        /// - Parameters:
+        ///   - major: The major version number.
+        ///   - minor: The minor version number.
+        ///   - patch: The patch version number.
+        public init(major: Int, minor: Int, patch: Int) {
+            self.major = major
+            self.minor = minor
+            self.patch = patch
+        }
+        
+        /// Initializes a `Version` instance from an `OperatingSystemVersion` object.
+        ///
+        /// - Parameter version: An `OperatingSystemVersion` instance from which to derive the `Version` instance.
+        public init(_ version: OperatingSystemVersion) {
+            self.major = version.majorVersion
+            self.minor = version.minorVersion
+            self.patch = version.patchVersion
+        }
     }
+    
 }
