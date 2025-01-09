aed9e7bc7cb773418eba7bd8bea1356e0067b22b
iOmega8561 <giusepperocco38@gmail.com>
2025-01-09 15:59:51 +0100

    Sevaral changes: - Renamed VZKitGraphicalConsole protocol -> VZKitPreviewableComponent - Improved documentation for VZKitPreviewableComponent - Renamed GraphicalConsole -> VZKitConsoleView - Renamed Framebuffer -> VZKitConsoleFramebuffer - Moved VZKitConsoleFramebuffer out of VZKitConsoleView scope

---
+++ VirtualizationKit/Implementations/PublicAPI/VZKitConsoleFramebuffer.swift
@@ -0,0 +1,94 @@
+//
+//  VZKitConsoleFramebuffer.swift
+//  VirtualizationKit
+//
+//  Created by Giuseppe Rocco on 15/10/24.
+//
+
+import AppKit
+
+import Virtualization
+
+
+/// A custom implementation of `VZVirtualMachineView`.
+///
+/// `VZKitConsoleFramebuffer` serves as a custom view for displaying virtual machine output, providing a replacement for
+/// `VZVirtualMachineView` with added awareness of SwiftUI preview contexts. This class disables mouse interactions
+/// when in preview mode, ensuring the view doesn't capture inputs that could interfere with development previews.
+public final class VZKitConsoleFramebuffer: VZVirtualMachineView, VZKitPreviewableComponent {
+    
+    /// Indicates if the view is in a SwiftUI preview context, controlling whether it should capture user interactions.
+    ///
+    /// When `true`, mouse events are ignored to prevent interaction during Xcode Previews.
+    public var isPreviewContext: Bool = false
+    
+    /// Handles the `mouseUp` event, conditionally passing it to the superclass.
+    ///
+    /// - Parameter event: The mouse-up event to handle.
+    ///
+    /// If `isPreviewContext` is `true`, this method suppresses the event to prevent interaction; otherwise, it
+    /// forwards the event to the superclass.
+    public override func mouseUp(with event: NSEvent) {
+        isPreviewContext ? () : super.mouseUp(with: event)
+    }
+    
+    /// Handles the `mouseDown` event, conditionally passing it to the superclass.
+    ///
+    /// - Parameter event: The mouse-down event to handle.
+    ///
+    /// If `isPreviewContext` is `true`, this method suppresses the event to prevent interaction; otherwise, it
+    /// forwards the event to the superclass.
+    public override func mouseDown(with event: NSEvent) {
+        isPreviewContext ? () : super.mouseDown(with: event)
+    }
+    
+    /// Handles the `mouseMoved` event, conditionally passing it to the superclass.
+    ///
+    /// - Parameter event: The mouse-move event to handle.
+    ///
+    /// If `isPreviewContext` is `true`, this method suppresses the event to prevent interaction; otherwise, it
+    /// forwards the event to the superclass.
+    public override func mouseMoved(with event: NSEvent) {
+        isPreviewContext ? () : super.mouseMoved(with: event)
+    }
+    
+    /// Handles the `mouseExited` event, conditionally passing it to the superclass.
+    ///
+    /// - Parameter event: The mouse-exit event to handle.
+    ///
+    /// If `isPreviewContext` is `true`, this method suppresses the event to prevent interaction; otherwise, it
+    /// forwards the event to the superclass.
+    public override func mouseExited(with event: NSEvent) {
+        isPreviewContext ? () : super.mouseExited(with: event)
+    }
+    
+    /// Handles the `mouseDragged` event, conditionally passing it to the superclass.
+    ///
+    /// - Parameter event: The mouse-dragged event to handle.
+    ///
+    /// If `isPreviewContext` is `true`, this method suppresses the event to prevent interaction; otherwise, it
+    /// forwards the event to the superclass.
+    public override func mouseDragged(with event: NSEvent) {
+        isPreviewContext ? () : super.mouseDragged(with: event)
+    }
+    
+    /// Handles the `mouseEntered` event, conditionally passing it to the superclass.
+    ///
+    /// - Parameter event: The mouse-entered event to handle.
+    ///
+    /// If `isPreviewContext` is `true`, this method suppresses the event to prevent interaction; otherwise, it
+    /// forwards the event to the superclass.
+    public override func mouseEntered(with event: NSEvent) {
+        isPreviewContext ? () : super.mouseEntered(with: event)
+    }
+    
+    /// Handles hit testing, returning `nil` if `isPreviewContext` is `true`.
+    ///
+    /// - Parameter point: The location to test within the view's coordinate system.
+    /// - Returns: The `NSView` at the specified point, or `nil` if `isPreviewContext` is `true`.
+    ///
+    /// If `isPreviewContext` is `true`, this method returns `nil` to ignore hits during preview.
+    public override func hitTest(_ point: NSPoint) -> NSView? {
+        isPreviewContext ? nil : super.hitTest(point)
+    }
+}
--- VirtualizationKit/Implementations/PublicAPI/VZKitConsoleView.swift
+++ VirtualizationKit/Implementations/PublicAPI/VZKitConsoleView.swift
@@ -1,5 +1,5 @@
 //
-//  GraphicalConsole.swift
+//  VZKitConsoleView.swift
 //  VirtualizationKit
 //
 //  Created by Giuseppe Rocco on 12/10/24.
@@ -11,8 +11,8 @@ import Virtualization
 
 /// A SwiftUI-compatible wrapper for an AppKit-based virtual machine console view.
 ///
-/// `GraphicalConsole` is a wrapper around an AppKit `NSView` that provides a graphical console for a virtual machine.
-/// It conforms to `VZKitGraphicalConsole` and can be used as a SwiftUI view through `NSViewRepresentable`.
+/// `VZKitConsoleView` is a wrapper around an AppKit `NSView` that provides a graphical console for a virtual machine.
+/// It conforms to `VZKitPreviewableComponent` and can be used as a SwiftUI view through `NSViewRepresentable`.
 /// This structure is designed to configure and present the virtual machine's display within a SwiftUI application.
 ///
 /// # Overview
@@ -21,15 +21,15 @@ import Virtualization
 /// - `automaticallyReconfiguresDisplay`: Determines if the console should adjust the display configuration based on window size.
 /// - `capturesSystemKeys`: Determines if the console should capture system-wide key combinations.
 ///
-/// `GraphicalConsole` is intended to be wrapped by a `ConsoleView` for direct use in SwiftUI layouts. The view is
+/// `VZKitConsoleView` is intended to be wrapped by a `ConsoleView` for direct use in SwiftUI layouts. The view is
 /// configurable based on whether it is running in a preview context, allowing for a customized experience in development
 /// previews.
 ///
-/// - Note: `GraphicalConsole` should only be used within a SwiftUI context and is not designed for standalone usage.
+/// - Note: `VZKitConsoleView` should only be used within a SwiftUI context and is not designed for standalone usage.
 ///
 /// - Parameters:
 ///   - Template: A type conforming to `VZKitTemplate`, which provides the necessary configuration for the virtual machine.
-public struct GraphicalConsole<Template: VZKitTemplate>: VZKitGraphicalConsole {
+public struct VZKitConsoleView<Template: VZKitTemplate>: NSViewRepresentable, VZKitPreviewableComponent {
     
     /// Indicates if the view is used in a preview context.
     ///
@@ -53,13 +53,13 @@ public struct GraphicalConsole<Template: VZKitTemplate>: VZKitGraphicalConsole {
     
     /// Creates and configures the `NSView` for this console view.
     ///
-    /// This method is part of the standard `NSViewRepresentable` protocol and initializes the `Framebuffer` instance,
-    /// binding the appropriate virtual machine and context state.
+    /// This method is part of the standard `NSViewRepresentable` protocol and initializes the
+    /// `VZKitConsoleFramebuffer` instance, binding the appropriate virtual machine and context state.
     ///
     /// - Parameter context: The context provided by `NSViewRepresentable`, which manages lifecycle and coordination.
-    /// - Returns: A configured instance of `Framebuffer` displaying the virtual machine's output.
-    public func makeNSView(context: Context) -> Framebuffer {
-        let vmView = Framebuffer()
+    /// - Returns: A configured instance of `VZKitConsoleFramebuffer` displaying the virtual machine's output.
+    public func makeNSView(context: Context) -> VZKitConsoleFramebuffer {
+        let vmView = VZKitConsoleFramebuffer()
         vmView.virtualMachine = vzVirtualMachine
         vmView.isPreviewContext = isPreviewContext
         return vmView
@@ -68,12 +68,13 @@ public struct GraphicalConsole<Template: VZKitTemplate>: VZKitGraphicalConsole {
     /// Updates the console view with new state or configuration changes.
     ///
     /// This method is called automatically when SwiftUI detects a state change, and it ensures that properties
-    /// such as `automaticallyReconfiguresDisplay` and `capturesSystemKeys` are synchronized with the `Framebuffer`.
+    /// such as `automaticallyReconfiguresDisplay` and `capturesSystemKeys`
+    /// are synchronized with the `VZKitConsoleFramebuffer`.
     ///
     /// - Parameters:
-    ///   - vmView: The existing `Framebuffer` view instance to update.
+    ///   - vmView: The existing `VZKitConsoleFramebuffer` view instance to update.
     ///   - context: The context provided by `NSViewRepresentable` for managing state and interactions.
-    public func updateNSView(_ vmView: Framebuffer, context: Context) {
+    public func updateNSView(_ vmView: VZKitConsoleFramebuffer, context: Context) {
         vmView.virtualMachine = vzVirtualMachine
         vmView.automaticallyReconfiguresDisplay = automaticallyReconfiguresDisplay
         vmView.capturesSystemKeys = capturesSystemKeys
