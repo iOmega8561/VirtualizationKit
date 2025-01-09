85ee3c557eb2121c1b4bfa8f3e603a45164cb64f
iOmega8561 <giusepperocco38@gmail.com>
2025-01-09 13:50:01 +0100

    Provided documentation for InternalAPI protocols

--- VirtualizationKit/Protocols/InternalAPI/VZKitConstructible.swift
+++ VirtualizationKit/Protocols/InternalAPI/VZKitConstructible.swift
@@ -16,7 +16,12 @@
 
 import Virtualization
 
+/// A protocol that defines a constructible type.
+///
+/// Types conforming to `VZKitConstructible` can specify an associated type
+/// that defaults to `Self`, enabling generic construction patterns.
 protocol VZKitConstructible {
     
+    /// The type of the object to be constructed.
     associatedtype Constructible = Self
 }
--- VirtualizationKit/Protocols/InternalAPI/VZKitStorageConstructible.swift
+++ VirtualizationKit/Protocols/InternalAPI/VZKitStorageConstructible.swift
@@ -14,15 +14,21 @@
 //  Created by Giuseppe Rocco on 09/01/25.
 //
 
+/// A protocol that extends `VZKitConstructible` for storage-based construction.
+///
+/// Types conforming to `VZKitStorageConstructible` provide a static factory method
+/// that supports creating instances based on a file location and input type.
 protocol VZKitStorageConstructible: VZKitConstructible {
-        
+    
+    /// The type of input required to create a new instance.
     associatedtype InputType
     
-    /// This is the standard factory method for any `VZKitStorageConstructible` conforming class.
-    /// It creates a block device based on the input parameters, and returns the attachment to the caller.
+    /// Creates a new instance of the conforming type at the specified location, using the specified input.
     ///
     /// - Parameters:
-    ///   - url: The location at which the disk image should be created on the host file system.
-    ///   - type: Mounting permissions with integer size of the virtual disk image, in gigabytes.
+    ///   - url: The file system URL where the instance should be created.
+    ///   - type: The input required to create the instance.
+    /// - Returns: A new instance of the conforming type.
+    /// - Throws: An error if the creation fails.
     static func create(at url: URL, type: InputType) throws -> Constructible
 }
