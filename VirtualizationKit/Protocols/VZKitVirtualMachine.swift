c4cfe0e443140adae8d1d100a12385cc1234ff85
iOmega8561 <giusepperocco38@gmail.com>
2025-01-10 10:19:41 +0100

    Several changes: - Fixed VZPlatformConfiguration.createMachineIdentifier generic parameter protocol - Removed @VZKitActor pinning from VZKitVirtualMachine, but pinned VZKitMacOSInstaller.restoreFromDiskImage to it

--- VirtualizationKit/Protocols/VZKitVirtualMachine.swift
+++ VirtualizationKit/Protocols/VZKitVirtualMachine.swift
@@ -21,7 +21,7 @@ public protocol VZKitVirtualMachine: Sendable {
     /// the developer wishes to support in their VM implementation. For example, commands might include:
     /// `.start`, `.stop`, `.pause`, and `.resume`. The specific cases are left to the developer’s discretion
     /// to suit the application’s needs.
-    associatedtype CommandIterable: CaseIterable
+    associatedtype CommandIterable: Sendable
     
     /// A type representing the template data required to configure the virtual machine.
     ///
@@ -56,17 +56,6 @@ public protocol VZKitVirtualMachine: Sendable {
     /// updated and accessible to observers, such as the UI.
     var stateManager: StateManagerType { get }
     
-    /// Creates a new virtual machine instance based on the specified template.
-    ///
-    /// This static factory method returns a result containing either a new instance of the conforming type or an error
-    /// if the creation process fails. By using `Result<Self, Error>`, this method provides a mechanism for handling
-    /// errors that may occur during VM creation, allowing the caller to take action (e.g., showing an error message)
-    /// if needed.
-    ///
-    /// - Parameter template: The data transfer object containing the configuration information for the VM.
-    /// - Returns: A `VZKitResult<Template>` containing either a new virtual machine instance or an error.
-    static func createMachine(_ template: Template) async -> VZKitResult<Template>
-    
     /// Sends a command to the virtual machine and updates the shared state accordingly.
     ///
     /// This method provides a standard way to interact with the `VZVirtualMachine` instance by sending specific commands,
@@ -74,12 +63,9 @@ public protocol VZKitVirtualMachine: Sendable {
     /// `stateManager` to reflect any changes to the VM’s state. In the event of an error, the state should be
     /// safely reset before the error is propagated.
     ///
-    /// - Important: This method is pinned to `@VZKitActor` to ensure serial execution of commands, which helps
-    ///   maintain consistency and avoid race conditions when controlling the VM.
-    ///
     /// - Parameter command: A `CommandIterable` case representing the command to be executed on the VM.
     /// - Throws: An error if the command execution fails, with the state reset as needed before propagation.
-    @VZKitActor func sendCommand(_ command: CommandIterable) async throws
+    func sendCommand(_ command: CommandIterable) async throws
     
     /// Attaches a removable USB disk to the virtual machine using a specified disk image.
     ///
@@ -87,16 +73,13 @@ public protocol VZKitVirtualMachine: Sendable {
     /// USB controllers. The specified disk image is used to create the device, which is then attached to the VM.
     /// The returned `UUID` can be used to manage the device, such as detaching it later.
     ///
-    /// - Important: This method is pinned to `@VZKitActor` to ensure serial execution of operations,
-    ///   maintaining consistency and preventing race conditions during device attachment.
-    ///
     /// - Parameter url: A `URL` pointing to the disk image file to be attached. The file can be in a read-only
     ///   or writable configuration, depending on implementation details.
     /// - Returns: A `UUID` uniquely identifying the attached USB device.
     /// - Throws: An error if the attachment fails, such as if the virtual machine does not support XHCI USB
     ///   controllers. Possibly other errors related to device creation or attachment.
     @available(macOS 15.0, *)
-    @VZKitActor func attachRemovableUSBDisk(usingImageAt url: URL) async throws -> UUID
+    func attachRemovableUSBDisk(usingImageAt url: URL) async throws -> UUID
 
     /// Detaches a previously attached removable USB disk from the virtual machine.
     ///
@@ -104,13 +87,10 @@ public protocol VZKitVirtualMachine: Sendable {
     /// The identifier must match a device previously attached using the `attachRemovableUSBDisk(usingImageAt:)` method.
     /// If the specified device is not found, an error is thrown.
     ///
-    /// - Important: This method is pinned to `@VZKitActor` to ensure serial execution of operations,
-    ///   maintaining consistency and avoiding race conditions during device detachment.
-    ///
     /// - Parameter id: The `UUID` identifying the USB device to be detached. This value should correspond to
     ///   a device previously attached to the VM.
     /// - Throws: Should throw an error if the detachment fails, such as iif the virtual machine does not support XHCI USB controllers,
     ///   or the supplied id doesn't match any device that is currently attached to the VM. Possibly also other errors related to the detachment process.
     @available(macOS 15.0, *)
-    @VZKitActor func detachRemovableUSBDisk(identifiedBy id: UUID) async throws
+    func detachRemovableUSBDisk(identifiedBy id: UUID) async throws
 }
