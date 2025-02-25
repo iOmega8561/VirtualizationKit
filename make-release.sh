42fa08c1b2f5de2dc38690923ad2e2ba01698850
iOmega8561 <giusepperocco38@gmail.com>
2025-02-25 01:24:15 +0100

    Fixed make-release.sh and removed signature from xcode project

--- make-release.sh
+++ make-release.sh
@@ -39,25 +39,50 @@ rm -rf ${ZIP_FILE}
 #     --team-id "${TEAM_ID}" \
 #     --password "${APP_PASSWORD}"
 
-echo "🛠️ Creating XCFramework..."
+echo "🛠 ️ Creating XCFramework..."
 xcodebuild -create-xcframework \
     -archive "${ARCHIVE_PATH}" \
     -framework "${FRAMEWORK_NAME}.framework" \
     -output "./${FRAMEWORK_NAME}.xcframework"
 
 echo "🔑 Signing XCFramework..."
-codesign --force --deep --sign "$CERTIFICATE_NAME" "${FRAMEWORK_NAME}.xcframework"
+find "${FRAMEWORK_NAME}.xcframework" -type f -perm +111 -exec \
+codesign --force --strict --options runtime --timestamp \
+--sign "$CERTIFICATE_NAME" {} \;
 
-echo "📦 Zipping XCFramework..."
-zip -r "${FRAMEWORK_NAME}.zip" "${FRAMEWORK_NAME}.xcframework"
-zip -j "${FRAMEWORK_NAME}.zip" "${SCRIPTPATH}/LICENSE"
+codesign --force \
+    --deep \
+    --strict \
+    --options runtime \
+    --timestamp \
+    --sign "$CERTIFICATE_NAME" \
+    "${FRAMEWORK_NAME}.xcframework"
+
+echo "📦 Packaging XCFramework..."
+mkdir -p DMG
+cp -R "${FRAMEWORK_NAME}.xcframework" DMG/
+cp "${SCRIPTPATH}/LICENSE" DMG/
+
+hdiutil create -volname "${FRAMEWORK_NAME}" \
+  -srcfolder ./DMG \
+  -format UDZO \
+  -ov "./${FRAMEWORK_NAME}.dmg"
+
+echo "🔑 Signing DMG Package..."
+codesign \
+    --force \
+    --sign "$CERTIFICATE_NAME" \
+    "${FRAMEWORK_NAME}.dmg"
 
 echo "📤 Submitting for Notarization..."
-xcrun notarytool submit "${FRAMEWORK_NAME}.zip" --keychain-profile "$KEYCHAIN_PROFILE" --wait
+xcrun notarytool submit "${FRAMEWORK_NAME}.dmg" \
+    --keychain-profile "$KEYCHAIN_PROFILE" \
+    --wait
 
 echo "📎 Stapling Notarization..."
-xcrun stapler staple "${FRAMEWORK_NAME}.zip"
+xcrun stapler staple "${FRAMEWORK_NAME}.dmg"
 
-rm "${FRAMEWORK_NAME}.xcframework"
+rm -fr DMG
+rm -fr "${FRAMEWORK_NAME}.xcframework"
 
 echo "✅ Done! XCFramework is notarized and ready for distribution."
